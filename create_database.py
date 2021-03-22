a9b7bad01d70459ed0095ef0177d10835fb81285
Guilherme <guirlviana@gmail.com>
2021-03-22 20:28:43 -0300

    ....

--- create_database.py
+++ create_database.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from sqlalchemy.dialects.sqlite import BLOB
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -11,7 +12,7 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://qxbazulxnsppso:d1ea93932f699a54e2b0ecf413b9309191c5b4dec83b7f5c983dc347b038e179@ec2-18-233-83-165.compute-1.amazonaws.com:5432/d4em91gppr7mcf'
 
 db = SQLAlchemy(app)  # Instancia o sqlalchemy usando as configurações acima
-
+# db: SQLAlchemy
 
 class Cliente(db.Model):
     __tablename__ = 'cliente'
@@ -23,7 +24,7 @@ class Cliente(db.Model):
     dataNasc = db.Column(db.DateTime)
     cpf = db.Column(db.String, unique=True)
     telefone = db.Column(db.String)
-    foto = db.Column(db.Binary)
+    foto = db.Column(BLOB)
     plano = db.Column(db.Integer)
     
 
@@ -38,7 +39,7 @@ class Autonomo(db.Model):
     dataNasc = db.Column(db.DateTime)
     cpf = db.Column(db.String, unique=True)
     telefone = db.Column(db.String)
-    foto = db.Column(db.Binary)
+    foto = db.Column(BLOB)
     plano = db.Column(db.Integer)
     categoria = db.Column(db.String)
     valorhora = db.Column(db.Numeric(precision=10, scale=2))
@@ -51,7 +52,7 @@ class Portfolio(db.Model):
     portfolio_id = db.Column(db.Integer, db.Sequence(
         'portfolio_id_auto_incremento', start=1), primary_key=True)
     id_usuario = db.Column(db.Integer)
-    foto = db.Column(db.Binary)
+    foto = db.Column(BLOB)
 
 class Classificacao(db.Model):
     __tablename__ = 'classificacao'
