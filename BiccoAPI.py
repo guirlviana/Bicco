ff5d937050b566d4ed0e2098ec2a012191190f9f
Guilherme <guirlviana@gmail.com>
2021-04-06 19:31:49 -0300

    adicionado type str

--- BiccoAPI.py
+++ BiccoAPI.py
@@ -31,7 +31,7 @@ def cadastrar_autonomo():
         pedidos = data['pedidos'] 
         descricao = str(data['descricao'] )
         avaliacao = data['avaliacao']
-        query = f"INSERT INTO autonomo VALUES ('" + nome + "', '" + email + "', '" + senha + "', " + datanasc + ", '" + cpf + "', '" + tel + "', " + foto + "," + plano + ", '" + categoria + "', " + preco + ", " + pedidos + ", '" + descricao + "'," + avaliacao + ")" 
+        query = f"INSERT INTO autonomo VALUES ('" + str(nome) + "', '" + str(email) + "', '" + str(senha) + "', " + str(datanasc) + ", '" + str(cpf) + "', '" + str(tel) + "', " + foto + "," + plano + ", '" + str(categoria) + "', " + preco + ", " + pedidos + ", '" + str(descricao) + "'," + avaliacao + ")" 
         db.session.execute(query)
         db.commit()
     except Exception as erro:
